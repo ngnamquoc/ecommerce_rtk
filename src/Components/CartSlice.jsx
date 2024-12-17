13f190c540dc67c59b5939b516b162ae55e313a7
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-12-16 22:07:30 -0500

    add basic shopping cart functionalities + supercoin feature

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,14 +1,11 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
     cartItems: [],
   };
-
-
-
 const CartSlice = createSlice({
-    name:'cart',
+    name: 'cart',
     initialState,
-    reducers:{
+    reducers: {
         addItemToCart(state, action) {
             const existingItem = state.cartItems.find(item => item.id === action.payload.id);
             if (existingItem) {
@@ -17,9 +14,33 @@ const CartSlice = createSlice({
               state.cartItems.push({ ...action.payload, quantity: 1 });
             }
           },
+          removeItemFromCart(state, action) {
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+          },
+          clearCart(state) {
+            state.cartItems = [];
+          },
+          increaseItemQuantity(state, action) {
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+              itemToIncrease.quantity += 1;
+            }
+          },
+          decreaseItemQuantity(state, action) {
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+              itemToDecrease.quantity -= 1;
+            }
+          },
+
 
     }
- 
 });
-
-
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+  } = CartSlice.actions;
+  export default CartSlice.reducer;
